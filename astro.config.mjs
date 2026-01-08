69ff3b5d10cc40eca16edf808ba1231f041c5ed6
jhmwkg <thejoepage@gmail.com>
2026-01-08 16:41:10 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, envField } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  env: {
+    schema: {
+      CONTENT_ISLAND_SECRET_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+        default: "INFORM_VALID_TOKEN",
+      }),
+    },
+  },
+});
